cc60656a62ccc1ab3f2be92b8219a9db1a16d6b0
lanzhao <lanzhao_bimworks@163.com>
2018-01-18 21:35:23 +0800

    添加主要代码

---
+++ TiKu/TiKu/SQLiteManager.swift
@@ -0,0 +1,18 @@
+//
+//  SQLiteManager.swift
+//  TiKu
+//
+//  Created by tiny on 2018/1/18.
+//  Copyright © 2018年 tiny. All rights reserved.
+//
+
+import UIKit
+
+
+
+class SQLiteManager: NSObject {
+    
+    static let sharInstance:SQLiteManager = SQLiteManager()
+    
+    
+}
---
+++ TiKu/TiKu/SuperViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SuperViewController.swift
+//  TiKu
+//
+//  Created by tiny on 2018/1/18.
+//  Copyright © 2018年 tiny. All rights reserved.
+//
+
+import UIKit
+
+class SuperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
